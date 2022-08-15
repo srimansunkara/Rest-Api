916f0ca902e69a1b0cdf4ccaf9628a792b03bfa9
srimansunkara <srimannarayanasunkara@gmail.com>
2022-08-15 19:47:57 +0530

    all done

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/rest-api/handler"
+)
 
 func main() {
-	fmt.Println("hello")
+
+	r := mux.NewRouter().StrictSlash(true)
+	r.HandleFunc("/allgroceries", handler.AllGroceries)                        // ----> To request all groceries
+	r.HandleFunc("/groceries/{name}/{quantity}", handler.SingleGrocery)        // ----> To request a specific grocery
+	r.HandleFunc("/groceries", handler.GroceriesToBuy).Methods("POST")         // ----> To add  new grocery to buy
+	r.HandleFunc("/groceries/{name}", handler.UpdateGrocery).Methods("PUT")    // ----> To update a grocery
+	r.HandleFunc("/groceries/{name}", handler.DeleteGrocery).Methods("DELETE") // ----> Delete a grocery
+	log.Fatal(http.ListenAndServe(":10000", r))
 }
